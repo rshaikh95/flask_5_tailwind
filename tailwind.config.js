5c5c80207f7f93d386771817f2ec7f68f1eddf98
rshaikh95 <itsrahil@live.com>
2023-11-02 02:12:08 +0000

    Add existing project files to Git

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/home/rahil_shaikh/flask-app-video/templates/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
